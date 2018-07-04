cfd776903fb1afc8345a3dac006794679e69830d
Roman Artemenko <RGArtemenko@gmail.com>
2018-07-05 01:41:44 +0300

    started implement main view

---
+++ article_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+from .views import ArticleLiveListView
+
+urlpatterns = [
+    # path('', views.index, name='index'),
+    path('', ArticleLiveListView.as_view(), name='index'),
+    # path('list', views.catalog, name='list_short_url'),
+    # path('<int:shortly_id>/detail', views.detail, name='detail'),
+    # path('<int:shortly_id>', views.follow_link, name='follow_link'),
+    # path('new', views.new, name='new'),
+]
