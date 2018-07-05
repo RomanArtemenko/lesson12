a78091fca912f6b62b544a2ba671a54ca248d606
Roman Artemenko <RGArtemenko@gmail.com>
2018-07-06 01:42:53 +0300

    added some changes into model

--- article_app/models.py
+++ article_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -12,17 +13,34 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class ArticleManager(models.Manager):
+    def live(self):
+        return super().get_queryset().filter(state=Article.STATE_LIVE).order_by('timestamp_create')
+
+    def review(self):
+        return super().get_queryset().filter(state=Article.STATE_REVIEW).order_by('timestamp_create')
+
+    def canceled(self):
+        return super().get_queryset().filter(state=Article.STATE_CANCELED).order_by('timestamp_create')
+        
 class Article(models.Model):
+    STATE_REVIEW = 0
+    STATE_CANCELED = 25
+    STATE_LIVE = 100 
     CHOICE_STATE = (
-        ('REVIEW', 'На модерации'),
-        ('LIVE', 'Опубликована'),
-        ('CANCELED', 'Отклонена'),
+        (STATE_REVIEW, 'На модерации'),
+        (STATE_LIVE, 'Опубликована'),
+        (STATE_CANCELED, 'Отклонена'),
     )
-    category_id = models.ForeignKey(Category, on_delete=models.PROTECT)
-    header = models.CharField(max_length=50)
-    text = models.CharField(max_length=1024)
-    state = models.CharField(choice=CHOICE_STATE, default='REVIEW')
+    category_id = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='articles')
+    header = models.CharField(max_length=50, unique=True)
+    text = models.TextField()
+    state = models.SmallIntegerField(choices=CHOICE_STATE, default=STATE_REVIEW)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT, related_name='articles')
     timestamp_create = models.DateTimeField(auto_now_add=True)
+    timestamp_update = models.DateTimeField(auto_now=True)
+
+    objects = ArticleManager()
 
     class Meta:
         verbose_name = 'Статья'
--- article_app/views.py
+++ article_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Article
+
 
 # Create your views here.
+# def index(request):
+#     return render(request, 'article_app/index.html', {})
 
-class ArticleLiveListView()
-    pass
+class ArticleLiveListView(ListView):
+    queryset = Article.objects.live()
+    template_name = 'article_app/index.html'
